2717ecd76e5cd063f7dd6f5ff206a4655d3c9213
sirwolfanimation <159640694+sirwolfanimation@users.noreply.github.com>
2025-02-01 18:55:56 +0000

    added new page

--- hsccinfo/app.js
+++ hsccinfo/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var bdpahistoryRouter = require('./routes/bdpahistory');
 
 var app = express();
 
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/bdpahistory', bdpahistoryRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
